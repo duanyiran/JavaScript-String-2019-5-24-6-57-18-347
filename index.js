e7fa15fb10e036f1bcf8242abee7b2da314704b2
duanyiran <51947332+duanyiran@users.noreply.github.com>
2019-07-10 18:33:45 +0800

    dyr作业
    dyr作业

--- index.js
+++ index.js
@@ -4,13 +4,25 @@
 // 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
 var name = 'hello';
 // TODO write your code here...
+document.write(name.toUpperCase())
 
 
 // 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
+function titleCase(str) {
+  var array = str.toLowerCase().split(" ");
+  for (var i = 0; i < array.length; i++){
+    array[i] = array[i][0].toUpperCase() + array[i].substring(1, array[i].length);
+  }
+  var string = array.join(" ");
+  return string;
+}
+document.write(sentence.titleCase())
+
 
 
 // 完成下面程序，将money变量中的金额提取出来，输出：20。
 var money = '￥20';
 //TODO write your code here...
+document.write(money.substring(2))
